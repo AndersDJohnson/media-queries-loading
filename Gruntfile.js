d25344149f9cd658301fd7407287da7b691a3c6e
Anders D. Johnson <anders.d.johnson.developer@gmail.com>
2014-03-28 23:25:50 -0500

    add Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+
+    less: {
+      dist: {
+        files: {
+          'dist/media-queries-loading.css': 'src/media-queries-loading.less'
+        }
+      }
+    },
+
+    cssmin: {
+      dist: {
+        expand: true,
+        cwd: 'dist',
+        src: ['*.css', '!*.min.css'],
+        dest: 'dist',
+        ext: '.min.css'
+      }
+    }
+
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('default', ['less', 'cssmin'])
+};
